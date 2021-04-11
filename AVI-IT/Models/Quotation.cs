18c576a34a4e6b08054c9f8ee53d5b7ae5658f5d
DavySung <70301107+DavySung@users.noreply.github.com>
2021-04-12 01:53:12 +1000

    Add project files.

---
+++ AVI-IT/Models/Quotation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AVI_IT.Models
+{
+    public class Quotation
+    {
+        [Display(Name ="First Name")]
+        public string firstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Display(Name = "Description")]
+        public string description { get; set; }
+    }
+}
